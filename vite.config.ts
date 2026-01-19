8498654d62d12d8dbc1c2e61c614171a9ee0dcf3
Sijal Manandhar <sijal.m06@gmail.com>
2026-01-19 22:16:06 +0545

    add deploy.yml for Bun and update vite.config base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/typing-speed-test/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
